8554d5968d457d8d27aa31402aa2518e195bbfe8
Jason Rush <diminish7@gmail.com>
2012-03-09 12:26:59 -0700

    Refactored events page to remove the periodic refresh
    This didn't match the other screens. Moving toward having a menu option
    to manually refresh. Or leaving and returning to the screen refreshes

--- src/com/rushdevo/gitdroid/ui/fragments/BaseEventsFragment.java
+++ src/com/rushdevo/gitdroid/ui/fragments/BaseEventsFragment.java
@@ -1,17 +1,13 @@
 package com.rushdevo.gitdroid.ui.fragments;
 
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
-import java.util.Timer;
-import java.util.TimerTask;
 import java.util.TreeMap;
 
 import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -29,16 +25,8 @@ import com.rushdevo.gitdroid.utils.NonConfigurationChangeData;
  */
 public abstract class BaseEventsFragment extends BaseFragment {
 	private EventService service;
-	private Integer page;
 	
 	private EventsAdapter adapter;
-	
-	private Timer requeryTimer;
-	private static final long REQUERY_PERIOD = 1000*60*5;	// 5 Minutes
-	
-	private static final int SHOW_SPINNER_MESSAGE = 1;
-	private static final int HIDE_SPINNER_MESSAGE = 2;
-	private static final int SET_EVENT_LIST_MESSAGE = 3;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -48,48 +36,21 @@ public abstract class BaseEventsFragment extends BaseFragment {
 	}
 	
 	@Override
-	public void onResume() {
-		super.onResume();
-		if (getLastQueried() != null) {
-			// Reset the requery timer
-			// Requery every REQUERY_PERIOD
-			// Start the initial requery based on how long we've been paused for
-			Long current = Calendar.getInstance().getTimeInMillis();
-			Long diff = current - getLastQueried();
-			Long nextQuery = REQUERY_PERIOD - diff;
-			if (nextQuery < 0) nextQuery = 0L;
-			requeryTimer = new Timer();
-			requeryTimer.schedule(new PeriodicRequeryTask(), nextQuery, REQUERY_PERIOD);
-		}
-	}
-	
-	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		return inflater.inflate(R.layout.events, container, false);
-	}
-	
-	@Override
-	public void onPause() {
-		super.onPause();
-		// Don't fire the requery timer in the background
-		if (requeryTimer != null) {
-			requeryTimer.cancel();
-			requeryTimer = null;
-		}
 	}
 	
 	@Override
 	protected void initializeData() {
 		if (viewIsReady()) initializeView();
 		else {
-			if (requeryTimer == null) requeryTimer = new Timer();
-			requeryTimer.schedule(new PeriodicRequeryTask(), 0, REQUERY_PERIOD);
+			new QueryEventsTask().execute();
 		}
 	}
 	
 	@Override
 	protected boolean viewIsReady() {
-		return (getEvents() != null && !getEvents().isEmpty()) || requeryTimer != null;
+		return getEvents() != null && !getEvents().isEmpty();
 	}
 	
 	@Override
@@ -111,60 +72,36 @@ public abstract class BaseEventsFragment extends BaseFragment {
 	
 	public abstract void setLastQueried(Long lastQueried);
 	
-	public Integer getPage() {
-		if (page == null) page = 1;
-		return page;
-	}
-	
 	public EventService getEventServiceInstance() {
 		if (service == null) service = new EventService(getActivity());
 		return service;
 	}
 	
+	public Integer getPage() {
+		// TODO: Stubbed for future pagination
+		return 1;
+	}
+	
 	///////////// INNER CLASSES ////////////////////////
-	/**
-	 * Handler for executing UI tasks from within a TimerTask
-	 */
-	private Handler handler = new Handler() {
-    	@Override
-    	public void handleMessage(Message message) {
-    		switch(message.what) {
-    		case SHOW_SPINNER_MESSAGE:
-    			showSpinner(R.id.news_feed_container);
-    			break;
-    		case HIDE_SPINNER_MESSAGE:
-    			hideSpinner(R.id.news_feed_container);
-    			break;
-    		case SET_EVENT_LIST_MESSAGE:
-    			adapter.setEvents(getEvents());
-				adapter.notifyDataSetChanged();
-				break;
-    		}
-    	}
-    };
-    
-	/**
-	 * Timer task for requerying the event feed every 5 minutes while the activity is running 
-	 */
-	private class PeriodicRequeryTask extends TimerTask {
-		@Override
-		public void run() {
-			// Show the spinner
-			handler.sendEmptyMessage(SHOW_SPINNER_MESSAGE);
-			// Query the event feed
-			if (getActivity() != null) { // In case this gets called after the activity is detached
-				retrieveEvents();
-				// Download avatar images for each event
-				retrieveAvatarDrawables(getEvents());
-				handler.sendEmptyMessage(SET_EVENT_LIST_MESSAGE);
-				setLastQueried(Calendar.getInstance().getTimeInMillis());
-			}
-			
-			// Hide the spinner
-			handler.sendEmptyMessage(HIDE_SPINNER_MESSAGE);
+	private class QueryEventsTask extends AsyncTask<Void, Void, Void> {
+		protected void onPreExecute() {
+			showSpinner(R.id.news_feed_container);
 		}
 		
-		private void retrieveAvatarDrawables(List<Event> events) {
+	     protected Void doInBackground(Void... args) {
+	         retrieveEvents();
+	         // Download avatar images for each event
+			retrieveAvatarDrawables(getEvents());
+	         return null;
+	     }
+
+	     protected void onPostExecute(Void arg) {
+	    	 adapter.setEvents(getEvents());
+	    	 adapter.notifyDataSetChanged();
+	    	 hideSpinner(R.id.news_feed_container);
+	     }
+	     
+	     private void retrieveAvatarDrawables(List<Event> events) {
 			Map<String, List<Event>> eventsByLogin = new TreeMap<String, List<Event>>();
 			// Group receivedEvents by user so just one call per user
 			for (Event event : events) {
@@ -191,5 +128,5 @@ public abstract class BaseEventsFragment extends BaseFragment {
 				}
 			}
 		}
-	}
+	 }
 }
